f9221a0c5d3e7cc6dd0c1fd52fd3e004955f2954
oumamoringa <richard.ouma@student.moringaschool>
2022-08-23 17:51:47 +0300

    Test passed

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    const text = document.querySelector("#text").innerHTML="This is really cool!"
+    console.log("The DOM has loaded" );
+  });
